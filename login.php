b5ad9270c24e58bb2b32b181b2ac53c58c8d4981
saima-mim <saimarahmanmim02@gmail.com>
2026-07-28 02:32:00 +0600

    Added login page

---
+++ login.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+<div class="login-container">
+
+    <form id="login-form">
+
+        <h2>Online Medical Appointment System</h2>
+
+        <h3>Login</h3>
+
+        <label for="email">Email</label>
+
+        <input
+            type="email"
+            id="email"
+            placeholder="Enter your email"
+            required>
+
+        <label for="password">Password</label>
+
+        <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            required>
+
+        <button type="button">
+            Login
+        </button>
+
+        <p class="success">Demo Login Form</p>
+
+    </form>
+
+</div>
+
+</body>
+</html>
